1362ffd966f7c93b8a8bd6a6ff0ec8468061f9e0
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-04-26 12:56:46 +0100

    Nato Alphabet tests for my own use

---
+++ src/test/kotlin/nato/NatoAlphabetTest.kt
@@ -0,0 +1,23 @@
+package nato
+
+import Resources
+import org.junit.Test
+
+class NatoAlphabetTest {
+
+    @Test
+    fun `show nato alphabet list`() {
+        val natoAlphabetList = Resources.resourceAsList("natoAlphabet.txt")
+
+        natoAlphabetList.forEach { println(it) }
+    }
+
+    @Test
+    fun `show nato alphabet list for a given word`() {
+        val natoAlphabetMap = Resources.resourceAsList("natoAlphabet.txt").groupBy { it.toLowerCase().first() }
+
+        val words = "My name"
+
+        words.toLowerCase().forEach { println(natoAlphabetMap[it] ?: " ") }
+    }
+}
