8fb7bd3dd98f6fd864d05f595a7400cae9cf4615
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2021-01-04 12:42:00 +0000

    Advent of Code 2020 Day 6 part 2

--- src/test/kotlin/adventofcode/2020/Day6Test.kt
+++ src/test/kotlin/adventofcode/2020/Day6Test.kt
@@ -20,11 +20,31 @@ class Day6Test {
 
     @Test
     fun `part 1`() {
-        assertEquals(6768, input.map { it.joinToString(separator = "").toCharArray().distinct().count() }.sum())
+        assertEquals(
+            6768,
+            input.map {
+                it.joinToString(separator = "")
+                    .toCharArray()
+                    .distinct()
+                    .count()
+            }.sum()
+        )
     }
 
     @Test
     fun `part 2`() {
-
+        assertEquals(
+            3489,
+            input.fold(0, { acc: Int, list: List<String> ->
+                if (list.size == 1) acc + list.first().count()
+                else {
+                    val head = list.first().toCharArray().toList()
+                    val tail = list.drop(1)
+                    acc + head.fold(0, { count: Int, c: Char ->
+                        if (tail.all { it.contains(c) }) count.inc() else count
+                    })
+                }
+            })
+        )
     }
 }
