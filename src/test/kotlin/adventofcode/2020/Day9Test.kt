70fe8d936188ddc5eea83c13879efcf18d765be4
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2021-01-17 16:27:41 +0000

    Advent of Code 2020 Day 9 part 2

--- src/test/kotlin/adventofcode/2020/Day9Test.kt
+++ src/test/kotlin/adventofcode/2020/Day9Test.kt
@@ -1,10 +1,11 @@
 package adventofcode.`2020`
 
 import Resources
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import kotlin.math.absoluteValue
 
-private const val INVALID_INDEX = -1
+private const val INVALID_INDEX = -1L
 
 class Day9Test {
 
@@ -14,12 +15,37 @@ class Day9Test {
 
     @Test
     fun `part 1`() {
-        println(checkEncoding(input, 25))
+        assertEquals(104054607, checkEncoding(input, 25))
+    }
+
+    @Test
+    fun `part 2`() {
+        val element = checkEncoding(input, 25)
+        val invalidNumberIndex = input.indexOf(element)
+        val inputWithoutInvalidNumber = input.apply {
+            toMutableList().removeAt(invalidNumberIndex)
+        }
+        assertEquals(13935797, findEncryptionWeakness(inputWithoutInvalidNumber, element))
+    }
+
+    private tailrec fun findEncryptionWeakness(input: List<Long>, element: Long): Long {
+        var sum = 0L
+        val list = input.takeWhile {
+            sum += it
+            sum <= element
+        }
+        return if (list.sum() == element) {
+            list.max()?.let { max ->
+                list.min()?.let { min ->
+                    min + max
+                }
+            } ?: INVALID_INDEX
+        } else findEncryptionWeakness(input.drop(1), element)
     }
 }
 
 private tailrec fun checkEncoding(input: List<Long>, preamble: Int): Long {
-    val number = input.getOrNull(preamble) ?: return INVALID_INDEX.toLong()
+    val number = input.getOrNull(preamble) ?: return INVALID_INDEX
     val preambleSet = input.asSequence().take(preamble).toSet()
     return if (preambleSet.none { n -> preambleSet.any { (number - n - it).absoluteValue == 0L } }) number
     else checkEncoding(input.drop(1), preamble)
