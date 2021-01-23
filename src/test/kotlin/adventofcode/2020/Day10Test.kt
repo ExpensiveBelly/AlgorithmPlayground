0ce7c6362db64f326e5e4e3fe6a7d040bbdcf9d3
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2021-01-23 18:54:18 +0000

    Advent of Code 2020 Day 10 part 2

--- src/test/kotlin/adventofcode/2020/Day10Test.kt
+++ src/test/kotlin/adventofcode/2020/Day10Test.kt
@@ -17,6 +17,49 @@ class Day10Test {
         assertEquals(2176, differences.oneJoltDifferences * differences.threeJoltsDifferences)
     }
 
+    @Test
+    fun `part 2 copied`() {
+        val adapters: List<Int> = input.plus(0).plus(input.max()!! + 3).sorted()
+        val pathsByAdapter: MutableMap<Int, Long> = mutableMapOf(0 to 1L)
+        adapters.drop(1).forEach { adapter ->
+            pathsByAdapter[adapter] = (1..3).map { lookBack ->
+                pathsByAdapter.getOrDefault(adapter - lookBack, 0)
+            }.sum()
+        }
+
+        assertEquals(18512297918464, pathsByAdapter.getValue(adapters.last()))
+    }
+
+    @Test
+    fun `part 2`() {
+        assertEquals(18512297918464, countCombinations(input, 0))
+    }
+
+    private fun countCombinations(
+        input: List<Int>,
+        currentJolt: Int,
+        acc: Long = 0,
+        cache: MutableMap<Int, Long> = mutableMapOf()
+    ): Long = if (input.isEmpty()) acc
+    else {
+        val cachedValue = cache[currentJolt]
+        if (cachedValue != null) cachedValue.also { println("Cache hit: $currentJolt : $cachedValue") }
+        else {
+            val newJolts = input.filter { it in (currentJolt..currentJolt + 3) }
+            if (newJolts.isEmpty()) acc.inc().also { cache[currentJolt] = it }
+            else newJolts.sumByLong { countCombinations(input.toMutableList().apply { remove(it) }, it, acc, cache) }
+                .also { cache[currentJolt] = it }
+        }
+    }
+
+    private inline fun <T> Iterable<T>.sumByLong(selector: (T) -> Long): Long {
+        var sum = 0L
+        for (element in this) {
+            sum += selector(element)
+        }
+        return sum
+    }
+
     private tailrec fun accumulateDifferences(
         input: List<Int>,
         currentJolt: Int,
