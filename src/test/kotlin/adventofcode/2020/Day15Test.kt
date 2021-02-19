04bd365ff737ba45dd67baef842dddf767fcb2e4
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2021-02-19 20:45:14 +0000

    Advent of Code 2020 Day 15 part 2

--- src/test/kotlin/adventofcode/2020/Day15Test.kt
+++ src/test/kotlin/adventofcode/2020/Day15Test.kt
@@ -1,5 +1,6 @@
 package adventofcode.`2020`
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 
@@ -9,13 +10,27 @@ class Day15Test {
 
     @Test
     fun `part 1`() {
-        println(generateSequence(input) {
-            it + listOf(it.nextNumber())
-        }.take(2020 - (input.size - 1)).toList().last().last())
+        assertEquals(1085, input.generateSpokenNumber(2020))
+    }
+
+    @Test
+    fun `part 2`() {
+        assertEquals(10652, input.generateSpokenNumber(30_000_000))
+    }
+
+    private fun List<Int>.generateSpokenNumber(n: Int): Int {
+        val lastSpokenMap = mutableMapOf<Int, Int>()
+        dropLast(1).forEachIndexed { index, lastSpokenNumber -> lastSpokenMap[lastSpokenNumber] = index + 1 }
+        return generateSequence(size to last()) { (turn, lastNumberSpoken) ->
+            val lastPosition = lastSpokenMap[lastNumberSpoken]
+            lastSpokenMap[lastNumberSpoken] = turn
+            if (lastPosition == null) turn.inc() to 0
+            else turn.inc() to (turn - lastPosition)
+        }.take(n - (size - 1)).last().second
     }
 }
 
-private fun List<Int>.nextNumber(): Int {
+private fun List<Int>.nextNumberTooSlow(): Int {
     val index = dropLast(1).indexOfLast { it == last() }
     return if (index == -1) 0 else size - (index + 1)
 }
