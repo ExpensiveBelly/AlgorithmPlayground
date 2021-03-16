f1d77f1f3e1d480467fa8eb8576a0aed73886b05
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2021-03-16 21:31:07 +0000

    Boggle DFS

--- src/main/kotlin/boggle/BoggleDfs.kt
+++ src/main/kotlin/boggle/BoggleDfs.kt
@@ -7,39 +7,43 @@ import algorithm.mutableMatrixOf
 
 private class BoggleDfs(private val dictionary: List<String>) {
 
-    private fun String.isWord() = dictionary.any { it.equals(this, ignoreCase = true) }
-
-    private fun findWordsUtil(
-        boggle: MutableMatrix<Char>,
-        visited: MutableMatrix<Boolean>,
-        i: Int,
-        j: Int,
-        str: String
-    ) {
-        visited[i, j] = true
-        val string = str + boggle[i, j]
-
-//        if (string.isWord())
-        println(string)
-
-        (i - 1 until i + 1).filter { it >= 0 && it < boggle.rows }.forEach { row ->
-            (j - 1 until j + 1).filter { it >= 0 && it < boggle.cols }.forEach { column ->
-                if (!visited[row, column]) {
-                    findWordsUtil(boggle, visited, row, column, string)
-                }
+    fun boggle(board: MutableMatrix<Char>): Boolean {
+        val seen = createMutableMatrix(board.cols, board.rows) { _, _ -> false }
+        for (i in 0 until board.rows) {
+            for (j in 0 until board.cols) {
+                boggleDfs(board, i, j, "", seen)
             }
         }
-
-        visited[i, j] = false
+        return true
     }
 
-    fun findWords(boggle: MutableMatrix<Char>) {
-        val visited = createMutableMatrix<Boolean>(boggle.rows, boggle.cols) { _, _ -> false }
-        for (row in 0 until boggle.rows) {
-            for (column in 0 until boggle.cols) {
-                findWordsUtil(boggle, visited, row, column, "")
+    private fun boggleDfs(
+        board: MutableMatrix<Char>,
+        colIndex: Int,
+        rowIndex: Int,
+        current: String,
+        seen: MutableMatrix<Boolean>
+    ) {
+        val string = current + board[colIndex, rowIndex]
+        seen[colIndex, rowIndex] = true
+        if (dictionary.contains(current)) println(current)
+
+        fun Pair<Int, Int>.isInBounds() =
+            !((first < 0).or(first >= board.rows).or(second < 0).or(second >= board.cols))
+
+        fun neighborCoordinatesOf(x: Int, y: Int) = arrayOf(
+            Pair(x - 1, y - 1), Pair(x, y - 1), Pair(x + 1, y - 1), Pair(x - 1, y),
+            Pair(x + 1, y), Pair(x - 1, y + 1), Pair(x, y + 1), Pair(x + 1, y + 1)
+        ).filter { it.isInBounds() }
+
+        val neighbors = neighborCoordinatesOf(colIndex, rowIndex)
+        for ((col, row) in neighbors) {
+            if (!seen[col, row]) {
+                boggleDfs(board, col, row, string, seen)
             }
         }
+
+        seen[colIndex, rowIndex] = false
     }
 }
 
@@ -47,8 +51,5 @@ fun main() {
     val dictionary = listOf("GEEKS", "FOR", "QUIZ", "GUQ", "EE", "SEEK")
 
     val board = mutableMatrixOf(3, 3, 'G', 'I', 'Z', 'U', 'E', 'K', 'Q', 'S', 'E')
-    BoggleDfs(dictionary).findWords(board)
-
-//    val arrayOfArrays = arrayOf(charArrayOf('G', 'I', 'Z'), charArrayOf('U', 'E', 'K'), charArrayOf('Q', 'S', 'E'))
-//    Boggle.boggle(dictionary.toSet(), arrayOfArrays)
+    BoggleDfs(dictionary).boggle(board)
 }
