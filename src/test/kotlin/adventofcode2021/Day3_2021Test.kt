623410776d142c01443e82adde89b0abbd1eac5d
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2021-12-31 20:03:34 +0000

    Advent of Code 2021 day 3 part 2

--- src/test/kotlin/adventofcode2021/Day3_2021Test.kt
+++ src/test/kotlin/adventofcode2021/Day3_2021Test.kt
@@ -22,23 +22,59 @@ class Day3_2021Test {
 
     @Test
     fun `part 1`() {
-        val message = input.fold(
-            buildList {
-                addAll(input.first().map { BinaryCounter() })
-            }.toMutableList(),
-            { acc: MutableList<BinaryCounter>, s: String ->
-                acc.apply {
-                    s.forEachIndexed { index, c ->
-                        set(index, acc[index].inc(c))
-                    }
-                }
-            })
+        val binaryCounters = input.toBinaryCounters()
 
         assertEquals(
             3882564,
-            message.decimalNumber { it.max() } * message.decimalNumber { it.min() })
+            binaryCounters.decimalNumber { it.max() } * binaryCounters.decimalNumber { it.min() })
     }
 
-    private fun MutableList<BinaryCounter>.decimalNumber(transform: (BinaryCounter) -> Char): Long =
+    private fun List<BinaryCounter>.decimalNumber(transform: (BinaryCounter) -> Char): Long =
         map { transform(it) }.joinToString("").toDecimalNumber()
+
+    @Test
+    fun `part 2`() {
+        var filteredInputOxygenGenerator = input
+        var filteredInputCo2ScrubberRating = input
+        var oxygenGeneratorRatingFound = false
+        var co2ScrubberRatingFound = false
+        var i = 0
+        while (!oxygenGeneratorRatingFound || !co2ScrubberRatingFound) {
+            val max =
+                filteredInputOxygenGenerator.toBinaryCounters()[i].max() //This is very inefficient because it's calculated for the whole list rather than just for that column
+            val min = filteredInputCo2ScrubberRating.toBinaryCounters()[i].min()
+
+            if (!oxygenGeneratorRatingFound) {
+                filteredInputOxygenGenerator = filteredInputOxygenGenerator.filter { it[i] == max }
+                if (filteredInputOxygenGenerator.size == 1) oxygenGeneratorRatingFound = true
+            }
+            if (!co2ScrubberRatingFound) {
+                filteredInputCo2ScrubberRating =
+                    filteredInputCo2ScrubberRating.filter { it[i] == min }
+                if (filteredInputCo2ScrubberRating.size == 1) co2ScrubberRatingFound = true
+            }
+
+            i++
+        }
+        val oxygenGenerator = filteredInputOxygenGenerator.joinToString("")
+        val co2ScrubberRating = filteredInputCo2ScrubberRating.joinToString("")
+
+        assertEquals(
+            3385170,
+            (oxygenGenerator.toDecimalNumber() *
+                    co2ScrubberRating.toDecimalNumber())
+        )
+    }
+
+    private fun List<String>.toBinaryCounters() = fold(
+        buildList {
+            this.addAll(this@toBinaryCounters.first().map { BinaryCounter() })
+        }.toMutableList(),
+        { acc: MutableList<BinaryCounter>, s: String ->
+            acc.apply {
+                s.forEachIndexed { index, c ->
+                    set(index, acc[index].inc(c))
+                }
+            }
+        }).toList()
 }
