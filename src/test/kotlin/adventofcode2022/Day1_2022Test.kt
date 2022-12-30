f3bdc687283b9146f9a6e6e78520aa3c520ec285
ExpensiveBelly <lucascb84@gmail.com>
2022-12-30 14:44:32 +0000

    Advent of Code 2022 Day 1 using chunked

--- src/test/kotlin/adventofcode2022/Day1_2022Test.kt
+++ src/test/kotlin/adventofcode2022/Day1_2022Test.kt
@@ -1,32 +1,26 @@
 package adventofcode2022
 
-import Resources
-import junit.framework.Assert.assertEquals
+import adventofcode2020.Filename
+import adventofcode2020.readChunkedLines
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
 class Day1_2022Test {
 
-    private val input by lazy { Resources.resourceAsList("adventofcode2022/Day1_2022.txt") }
+    private val input: List<List<String>> by lazy { Filename("adventofcode2022/Day1_2022.txt").readChunkedLines() }
 
     @Test
     fun `Find the Elf carrying the most Calories`() {
         val caloriesCarried = calculateCaloriesCarriedByEachElf()
-        assertEquals(67027, caloriesCarried.maxOf { it })
+        assertEquals(67027, caloriesCarried.first())
     }
 
-    private fun calculateCaloriesCarriedByEachElf(): Collection<Int> {
-        var counter = 0
-        val caloriesCarried = mutableMapOf<Int, List<Int>>()
-        input.forEach { food: String ->
-            if (food.isEmpty()) counter++
-            else caloriesCarried[counter] =
-                (caloriesCarried[counter] ?: emptyList()) + food.toInt()
-        }
-        return caloriesCarried.mapValues { it.value.sum() }.values
+    private fun calculateCaloriesCarriedByEachElf(): List<Int> {
+        return input.map { calories -> calories.sumOf { it.toInt() } }.sortedDescending()
     }
 
     @Test
     fun `Find the top three Elves carrying the most Calories`() {
-        assertEquals(197291, calculateCaloriesCarriedByEachElf().sortedDescending().take(3).sum())
+        assertEquals(197291, calculateCaloriesCarriedByEachElf().take(3).sum())
     }
 }
