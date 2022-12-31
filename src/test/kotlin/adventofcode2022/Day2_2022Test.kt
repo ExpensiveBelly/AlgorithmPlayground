deb74220473048604831ab5b2c9561bc6cc9e4f1
ExpensiveBelly <lucascb84@gmail.com>
2022-12-31 22:21:33 +0000

    Advent of Code 2022 Day 2 Part 2

--- src/test/kotlin/adventofcode2022/Day2_2022Test.kt
+++ src/test/kotlin/adventofcode2022/Day2_2022Test.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 
 class Day2_2022Test {
 
-    private enum class RockPaperScissors(val score: Int) {
+    private enum class RockPaperScissors(val choiceValue: Int) {
         ROCK(1), PAPER(2), SCISSORS(3);
 
         companion object {
@@ -19,33 +19,71 @@ class Day2_2022Test {
         }
     }
 
+    private enum class Score(val value: Int) {
+        WIN(6), DRAW(3), LOSE(0);
+
+        companion object {
+            fun of(value: Char): Score = when (value) {
+                'X' -> LOSE
+                'Y' -> DRAW
+                'Z' -> WIN
+                else -> throw IllegalStateException()
+            }
+        }
+    }
+
+    private fun Score.against(rockPaperScissors: RockPaperScissors): Int = when (this) {
+        Score.WIN -> when (rockPaperScissors) {
+            RockPaperScissors.ROCK -> RockPaperScissors.PAPER
+            RockPaperScissors.PAPER -> RockPaperScissors.SCISSORS
+            RockPaperScissors.SCISSORS -> RockPaperScissors.ROCK
+        }
+
+        Score.DRAW -> rockPaperScissors
+        Score.LOSE -> when (rockPaperScissors) {
+            RockPaperScissors.ROCK -> RockPaperScissors.SCISSORS
+            RockPaperScissors.PAPER -> RockPaperScissors.ROCK
+            RockPaperScissors.SCISSORS -> RockPaperScissors.PAPER
+        }
+    }.choiceValue + value
+
     private fun RockPaperScissors.against(opponent: RockPaperScissors): Int = when (this) {
         RockPaperScissors.ROCK -> when (opponent) {
-            RockPaperScissors.ROCK -> 3
-            RockPaperScissors.PAPER -> 0
-            RockPaperScissors.SCISSORS -> 6
+            RockPaperScissors.ROCK -> Score.DRAW
+            RockPaperScissors.PAPER -> Score.LOSE
+            RockPaperScissors.SCISSORS -> Score.WIN
         }
 
         RockPaperScissors.PAPER -> when (opponent) {
-            RockPaperScissors.ROCK -> 6
-            RockPaperScissors.PAPER -> 3
-            RockPaperScissors.SCISSORS -> 0
+            RockPaperScissors.ROCK -> Score.WIN
+            RockPaperScissors.PAPER -> Score.DRAW
+            RockPaperScissors.SCISSORS -> Score.LOSE
         }
 
         RockPaperScissors.SCISSORS -> when (opponent) {
-            RockPaperScissors.ROCK -> 0
-            RockPaperScissors.PAPER -> 6
-            RockPaperScissors.SCISSORS -> 3
+            RockPaperScissors.ROCK -> Score.LOSE
+            RockPaperScissors.PAPER -> Score.WIN
+            RockPaperScissors.SCISSORS -> Score.DRAW
         }
-    } + score
+    }.value + choiceValue
 
     private val input by lazy { Resources.resourceAsList("adventofcode2022/Day2_2022.txt") }
 
     @Test
     fun `calculate the score you would get if you were to follow the strategy guide`() {
-        assertEquals(14297, input
-            .sumOf {
+        assertEquals(
+            14297,
+            input.sumOf {
                 RockPaperScissors.of(it.last()).against(RockPaperScissors.of(it.first()))
-            })
+            }
+        )
+    }
+
+    @Test
+    fun `what would your total score be if everything goes exactly according to your strategy guide`() {
+        assertEquals(
+            10498,
+            input.sumOf { Score.of(it.last()).against(RockPaperScissors.of(it.first())) }
+        )
     }
 }
